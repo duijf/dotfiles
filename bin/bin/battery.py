31005b90047f123c7db61e3473c411db6049da81
Laurens Duijvesteijn <l.duijvesteijn@gmail.com>
2015-12-19 19:32:54 +0100

    Add new battery script

--- bin/bin/battery.py
+++ bin/bin/battery.py
@@ -1,14 +1,32 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 
-import subprocess
+import subprocess, time
 from operator import methodcaller
 
-battery = subprocess.getoutput("acpi").split("\n")
-battery_items = list(map(methodcaller("split", " "), battery))
+minimum = 15
+pause   = 0.1
+colors  = ["520d0d", "590b0b", "630c0c", "630c0c", "590b0b", "520d0d"]
 
-res = "{} {}".format(battery_items[0][3], battery_items[1][3])
-res = res.replace('%', '')
-res = res.replace(',', '')
+capacity_cmd = "acpi -b | cut -d, -f2 | cut -d' ' -f2 | cut -d% -f1"
+charging_cmd = "acpi -b | cut -d' ' -f3 | cut -d, -f1"
+border_cmd = "chwb -c {} {}"
 
-print(res)
+def exec(command):
+    return subprocess.getoutput(command)
+
+while True:
+    capacity = map(int, exec(capacity_cmd).split("\n"))
+    charging = exec(charging_cmd).split("\n")
+
+    while sum(capacity) < minimum and "Charging" not in charging:
+        window = exec("pfw")
+
+        for c in colors:
+            exec(border_cmd.format(c, window))
+            time.sleep(0.1)
+
+        capacity = map(int, exec(capacity_cmd).split("\n"))
+        charging = exec(charging_cmd).split("\n")
+
+    time.sleep(5)
