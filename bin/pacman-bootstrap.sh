00c5850e29c5638221b05347dd525d8e411f05b6
Laurens Duijvesteijn <git@duijf.io>
2019-08-14 22:12:25 +0200

    bin: Add pacman bootstrap packages

---
+++ bin/pacman-bootstrap.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+sudo pacman -Syu
+sudo pacman -S  \
+	htop \
+	i3lock \
+	openssh \
+	openssh \
+	rxvt-unicode \
+	stow \
+	tar \
+	tree \
+	ufw \
+	unzip \
+	wget \
+	xcape \
+	zip \
+	zsh
