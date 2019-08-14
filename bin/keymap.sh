58bfd9e9a1d3cc07cc26c8309e1735b7fde9ba19
Laurens Duijvesteijn <git@duijf.io>
2019-08-14 22:46:04 +0200

    bin: Remove Xorg related packages

--- bin/keymap.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash
-setxkbmap -option 'caps:ctrl_modifier'
-xcape -e 'Caps_Lock=Escape'
--- bin/pacman-bootstrap.sh
+++ bin/pacman-bootstrap.sh
@@ -3,16 +3,12 @@
 sudo pacman -Syu
 sudo pacman -S  \
 	htop \
-	i3lock \
 	openssh \
-	openssh \
-	rxvt-unicode \
 	stow \
 	tar \
 	tree \
 	ufw \
 	unzip \
 	wget \
-	xcape \
 	zip \
 	zsh
