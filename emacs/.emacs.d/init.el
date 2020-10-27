0054d5e3be9030ea29623944a2a7ad2ab1ecec9a
Laurens Duijvesteijn <git@duijf.io>
2020-10-27 21:41:09 +0100

    emacs: Enable magit and evil keybindings

--- emacs/.emacs.d/init.el
+++ emacs/.emacs.d/init.el
@@ -165,3 +165,10 @@
       (push '(truncation nil nil) fringe-indicator-alist)))
 
   (minimap-mode))
+
+(use-package magit
+  :custom
+  (magit-display-buffer-function #'magit-display-buffer-same-window-except-diff-v1))
+
+(use-package evil-magit
+  :after magit)
