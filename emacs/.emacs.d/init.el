9e34b2a3568a0fc1c8f120a0ddf5363899c4ebf5
Laurens Duijvesteijn <git@duijf.io>
2020-12-07 20:50:35 +0100

    emacs: Disable all the annoying buffers

--- emacs/.emacs.d/init.el
+++ emacs/.emacs.d/init.el
@@ -218,3 +218,30 @@
   (evil-org-agenda-set-keys))
 
 (define-key evil-normal-state-map (kbd "C-a \\") 'split-window-right)
+
+;; Thanks to https://unix.stackexchange.com/a/152151
+;; Makes *scratch* empty.
+(setq initial-scratch-message "")
+
+;; Removes *scratch* from buffer after the mode has been set.
+(defun remove-scratch-buffer ()
+  (if (get-buffer "*scratch*")
+      (kill-buffer "*scratch*")))
+(add-hook 'after-change-major-mode-hook 'remove-scratch-buffer)
+
+;; Removes *messages* from the buffer.
+(setq-default message-log-max nil)
+(kill-buffer "*Messages*")
+
+;; Removes *Completions* from buffer after you've opened a file.
+(add-hook 'minibuffer-exit-hook
+      '(lambda ()
+         (let ((buffer "*Completions*"))
+           (and (get-buffer buffer)
+                (kill-buffer buffer)))))
+
+;; Don't show *Buffer list* when opening multiple files at the same time.
+(setq inhibit-startup-buffer-menu t)
+
+;; Show only one active window when opening multiple files at the same time.
+(add-hook 'window-setup-hook 'delete-other-windows)
