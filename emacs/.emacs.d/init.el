95da9b1a89eb0fd680d76edca00da9e06b9d3dfc
Laurens Duijvesteijn <git@duijf.io>
2020-12-21 15:57:01 +0100

    emacs: Fix evil-org keys in agenda

--- emacs/.emacs.d/init.el
+++ emacs/.emacs.d/init.el
@@ -295,9 +295,10 @@
   (add-hook 'org-mode-hook 'evil-org-mode)
   (add-hook 'evil-org-mode-hook
             (lambda ()
-              (evil-org-set-key-theme)))
-  (require 'evil-org-agenda)
-  (evil-org-agenda-set-keys))
+              (evil-org-set-key-theme))))
+
+(require 'evil-org-agenda)
+(evil-org-agenda-set-keys)
 
 (define-key evil-normal-state-map (kbd "C-a \\") 'split-window-right)
 
